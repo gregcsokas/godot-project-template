bf1440b7f9cc70e83ebf7dfa62901ed2c48d7ae9
Gergo Csokas <gergo.csokas@ericsson.com>
2021-12-25 18:45:04 +0100

    Initial commit

---
+++ KenguruHUN/hooks/post_gen_project.py
@@ -0,0 +1,12 @@
+import shutil
+
+from pathlib import Path
+from collections import OrderedDict  # noqa
+
+user_answers = dict(eval("{{cookiecutter}}"))
+
+if user_answers["gdnative_libs"] != "yes":
+    directory_path = Path().cwd() / 'gdnative_libs'
+    if directory_path.exists() and directory_path.is_dir():
+        shutil.rmtree(directory_path)
+
